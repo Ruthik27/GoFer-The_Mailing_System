6ceb99036b10f411b000dd598385297797b6c799
Ruthik27 <ruthikkale27@gmail.com>
2022-09-18 12:01:33 -0400

    updates

--- source code/files/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employee, Medicine, Component, Log
-# Register your models here.
-
-admin.site.register(Employee)
-admin.site.register(Medicine)
-admin.site.register(Component)
-admin.site.register(Log)
--- source code/server/email_info.py
+++
@@ -1,6 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS= True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER='impexdevmust@gmail.com'
-EMAIL_HOST_PASSWORD='impex_dev_must'
-EMAIL_PORT=587
